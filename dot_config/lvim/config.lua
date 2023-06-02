d5c8982394c2d8e4acfa16ad45703c05160af521
Jakob Widauer <jakob@auterion.com>
2023-06-02 15:20:34 +0200

    Update .config/lvim/config.lua

--- dot_config/lvim/config.lua
+++ dot_config/lvim/config.lua
@@ -2,24 +2,4 @@
 vim.opt.timeoutlen = 100
 vim.opt.relativenumber = true
 
-require("formatters")
-require("keymaps")
-require("lsp")
-require("ui")
-
----- Plugins
-lvim.plugins = {
-  require("plugins.copilot"),
-  require("plugins.crates"),
-  require("plugins.fidget"),
-  require("plugins.git-blame"),
-  require("plugins.minimap"),
-  require("plugins.neoscroll"),
-  require("plugins.nvim-bqf"),
-  require("plugins.onedark"),
-  require("plugins.rust-tools"),
-  require("plugins.symbols-outline"),
-  require("plugins.trouble"),
-  require("plugins.vim-surround"),
-  require("plugins.wilder")
-}
+require("user")
