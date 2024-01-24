dfc92e0aba631c2f099e1b7d2accd3b80947bc4e
jakob <jakob@auterion.com>
2024-01-24 11:25:19 +0100

    Update .config/lvim/lua/user/autocommands.lua

--- dot_config/lvim/lua/user/autocommands.lua
+++ dot_config/lvim/lua/user/autocommands.lua
@@ -8,7 +8,7 @@ lvim.autocommands = {
       local ft = vim.api.nvim_buf_get_option(0, "filetype")
       local opts = { buffer = vim.api.nvim_get_current_buf() }
 
-      if ft == 'cpp' then
+      if ft == 'cpp' or ft == 'c' then
         wk.register({
           ['go'] = { '<cmd>ClangdSwitchSourceHeader<cr>', 'Switch between source/header' },
         }, opts)
