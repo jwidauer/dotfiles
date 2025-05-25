6025367734ff5d595ecffba113f255c40b311cb3
Jakob Widauer <jakob.widauer@gmail.com>
2025-05-25 13:40:42 +0200

    Update .config/nvim/lua/community.lua

--- dot_config/nvim/lua/community.lua
+++ dot_config/nvim/lua/community.lua
@@ -5,12 +5,13 @@
 ---@type LazySpec
 return {
   "AstroNvim/astrocommunity",
-  { import = "astrocommunity.motion.nvim-surround" },
+  { import = "astrocommunity.diagnostics.trouble-nvim" },
   { import = "astrocommunity.motion.flash-nvim" },
+  { import = "astrocommunity.motion.nvim-surround" },
   { import = "astrocommunity.pack.bash" },
   { import = "astrocommunity.pack.cmake" },
   { import = "astrocommunity.pack.cpp" },
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.pack.rust" },
-  -- import/override with your plugins folder
+  { import = "astrocommunity.recipes.picker-lsp-mappings" },
 }
