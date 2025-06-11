7a46af00512b03a4bc0cfd58a261a2c181829a77
Jakob Widauer <jakob.widauer@gmail.com>
2025-06-11 14:44:52 +0200

    Update .config/nvim/lua/community.lua

--- dot_config/nvim/lua/community.lua
+++ dot_config/nvim/lua/community.lua
@@ -6,13 +6,14 @@
 return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.diagnostics.trouble-nvim" },
+  { import = "astrocommunity.git.git-blame-nvim" },
   { import = "astrocommunity.motion.flash-nvim" },
   { import = "astrocommunity.motion.nvim-surround" },
   { import = "astrocommunity.pack.bash" },
   { import = "astrocommunity.pack.cmake" },
   { import = "astrocommunity.pack.cpp" },
   { import = "astrocommunity.pack.lua" },
-  { import = "astrocommunity.pack.python-ruff" },
+  { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.pack.rust" },
   { import = "astrocommunity.recipes.picker-lsp-mappings" },
 }
